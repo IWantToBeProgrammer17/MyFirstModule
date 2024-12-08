00c73e526545d13551d56419bb52709bebd7e7a4
IWantToBeProgrammer17 <azky.haekal35@smp.belajar.id>
2024-12-08 12:29:57 +0700

    Initial
    Initial

---
+++ UseModuleEnglish.py
@@ -0,0 +1,7 @@
+import interactiveenglish as ie
+
+ie.daftar('Azky','Laki-laki','17','SMP','OrangTua','Islam','O8...','Desa...')
+ie.absensi("Azky","8 Desember")
+ie.upgradelevel("Azky","100",'6','7')
+ie.spp("Azky","7")
+ie.checkspp("Azky")
---
+++ interactiveenglish.py
@@ -0,0 +1,26 @@
+def daftar(nama, jeniskelamin, tanggal, asalsekolah, namaorangtua, agama , nomortelepon, alamat):
+        print("Nama             :"+nama)
+        print("Jenis Kelamin    :"+jeniskelamin)
+        print("Tanggal Lahir    :"+tanggal)
+        print("Asal Sekolah     :"+asalsekolah)
+        print("Nama OrangTua    :"+namaorangtua)
+        print("Agama            :"+agama)
+        print("Nomor Telepon    :"+nomortelepon)
+        print("Alamat           :"+alamat)
+
+def absensi(nama, tanggal):
+        print("nama             :"+nama)
+        print("tanggal          :"+tanggal)
+
+def upgradelevel(nama, score, lastlevel, nextlevel):
+    print("nama             :"+nama)
+    print("score            :"+score)
+    print("Last Level       :"+lastlevel)
+    print("Next Level       :"+nextlevel)
+
+def spp(nama, level):
+    print("Nama             :"+nama)
+    print("Level            :"+level)
+
+def checkspp(nama):
+    print("Nama             :"+nama)
